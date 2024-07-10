07a2a85b0f780ebb9a035506d115c1e5d855236a
kurtis <xk233qwq@163.com>
2024-07-10 19:25:28 +0800

    爬虫

--- src/spider/alpha_vantage/intraday.py
+++ src/spider/alpha_vantage/intraday.py
@@ -3,8 +3,9 @@ import requests
 res = requests.get(
     "https://www.alphavantage.co/query", params=dict(
         function='TIME_SERIES_INTRADAY',
-        symbol='1A001',
-        interval='60min',
+        symbol='COMP',
+        interval='1min',
+        outputsize='compact',
         apikey='UMW2GZUN5M5WBQUH'
     )
 )
